d17d4c6cecad0af7762a8850068d130a0b0ef570
{shiva} <girishiva93@gmail.com>
2022-01-21 21:14:37 +0545

    Learning show and hide, basic form

--- react-advance-2020/src/App.js
+++ react-advance-2020/src/App.js
@@ -7,7 +7,11 @@ import "./App.css";
 
 // import SetUp from "./tutorial/useEffect/setup/3-useEffect-fetch-data";
 
-import SetUp from "./tutorial/3- conditional-rendering/setup/2-short-circuit";
+// import SetUp from "./tutorial/3- conditional-rendering/setup/2-short-circuit";
+
+// import SetUp from "./tutorial/3- conditional-rendering/setup/3-show-hide";
+
+import SetUp from "./tutorial/4-forms/setup/1-controlled-inputs";
 
 function App() {
   return (
